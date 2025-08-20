e1271ea40a423de5045282bb005079c50f36023f
Iliya Iliev <iliyailiev3592@gmail.com>
2025-08-20 14:32:50 +0300

    reverse_engineering: Build binaries with clang
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

---
+++ reverse_engineering/src/b.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char *ptr_1 = (char *)malloc(1);
+    char *ptr_2 = NULL;
+    *ptr_2 = 'a';
+    //printf("%c\n", ptr[200000]);
+
+    return 0;
+}
