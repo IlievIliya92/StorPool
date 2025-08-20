604469ab2736514c44ee865266e5469e1d198d80
Iliya Iliev <iliyailiev3592@gmail.com>
2025-08-20 08:36:17 +0300

    reverse_engineering: Add reverse engineering task
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

---
+++ reverse_engineering/a.c
@@ -0,0 +1,24 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    const char *filename = "./pesho";
+    char buffer[1024];
+
+    int fd = open(filename, O_RDONLY);
+    /**
+     * Missing check for a valid fd
+     * The './pesho' file is missing on the file system or the relative path is
+     * incorrect.
+     */
+
+    ssize_t n = read(fd, buffer, sizeof buffer);
+    if (n == -1) {
+        return -1;
+    }
+    close(fd);
+
+    return 0;
+}
